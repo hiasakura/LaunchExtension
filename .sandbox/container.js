2f9b94798912844079a1f117d3141b701be52025
Adobe-Asakura <asakura@adobe.com>
2019-06-15 23:30:56 +0900

    Initial

---
+++ .sandbox/container.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "dataElements": {},
+  "rules": [],
+  "extensions": {},
+  "property": {
+    "settings": {
+      "domains": ["example.com"]
+    }
+  },
+  "company": {
+    "orgId": "ABCDEFGHIJKLMNOPQRSTUVWX@AdobeOrg"
+  },
+  "buildInfo": {
+    "turbineVersion": "25.5.0",
+    "turbineBuildDate": "2019-06-15T14:18:12.117Z",
+    "buildDate": "2019-06-15T14:18:12.117Z",
+    "environment": "development"
+  }
+}
